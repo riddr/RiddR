fd1c80fc37dcadfd964f18ca1ad0d7f4268dff76
Trajche Petrov <contact@skechboy.com>
2017-03-27 21:27:49 +0200

    Implemented synchronous get method and updated storage module for easier dynamic switch between multiple storage areas.

--- js/modules/storage.js
+++ js/modules/storage.js
@@ -14,26 +14,67 @@
 	// define global storage object 
 	this.storage = 
 	{
+		// set storage cache to avoid unnecessary chrome API hits 
+		cache : RiddR.defaults,
+
+		// set default storage area
+		area : chrome.storage.sync,
+
+		// synchronous get function returns value of 
+		get : function ()
+		{	
+			switch( typeof arguments[0] )
+			{
+				case 'object':
+					keys = {}
+					
+					for( key in arguments[0] )
+						keys[arguments[0][key]] = RiddR.defaults[arguments[0][key]];
+
+					return keys;
+				break;
+
+				case 'string':
+					return RiddR.defaults[arguments[0]];
+				break;
+
+				default:
+					RiddR.log('Storage get requires string or object input parameter')
+			}
+		},
+
 		// update an item in the storage object
 		set : function ()
 		{
 			// get input attributes 
 			data = ( typeof arguments[0] === 'object' )? arguments[0] : { [arguments[0]] : arguments[1] };
 			callback = ( typeof arguments[arguments.length-1] === 'function' )? arguments[arguments.length-1] : undefined;
-
-			chrome.storage.sync.set( data, callback );
+ 
+			this.area.set( data, callback );
 		},
 
 		// sync RiddR saved options
 		load : function ( callback )
 		{
-			chrome.storage.sync.get( RiddR.defaults, function ( options )
+			this.area.get( RiddR.defaults, function ( options )
 			{
 				RiddR.defaults = options; // update RiddR global defaults object
 
 				if( typeof callback === 'function' )
 					callback();
 			});
+		},
+
+		// remoe one or multiple items from the storage
+		remove : function ( keys, callback )
+		{
+			this.area.remove( keys, callback );
+		},
+
+		// remove all items from the storage
+		clear : function ( callbacl )
+		{
+			this.area.clear( callback );
 		}
 	}	
 
