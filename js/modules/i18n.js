62fbd5fb60fcd00061eb3e649ea372cd80502c39
Trajche Petrov <ceo@qbit.solutions>
2024-06-19 22:02:25 +0200

    Refactored the i18n module, to be compatible with v3 manifest

--- js/modules/i18n.js
+++ js/modules/i18n.js
@@ -9,146 +9,71 @@
  * @link		https://github.com/skechboy/RiddR
 */
 
-(function () 
+class i18n 
 {
-/*
- * ---------------------------------------------------------------------------------------------------------------------
- * Module private varialbes
- * ---------------------------------------------------------------------------------------------------------------------
-*/
-	this.locale = chrome.i18n.getUILanguage(); // get UI locale
 
-/*
- * ---------------------------------------------------------------------------------------------------------------------
- * Shortcut to Chrome's chrome.i18n internationalization API
- * ---------------------------------------------------------------------------------------------------------------------
-*/
-	this.__ = function( message_name , replace = [] )
-	{
-		if ( message = chrome.i18n.getMessage( message_name, replace ) )
-			return message;
-		else
-		{
-			// log missing locale messages
-			RiddR.log('Undefined message name: ' + message_name + ' in ' + RiddR.locale + ' locale!', 'warn');
-			return false;
-		}
-	}
-
-/*
- * ---------------------------------------------------------------------------------------------------------------------
- * Detect language from text input
- * ---------------------------------------------------------------------------------------------------------------------
-*/	
-	this.detect_lang = function ( text, callback, lang = 'auto' )
-	{
-		// pas input language into ouput variable
-		lang = { requested: lang };
-
-		chrome.i18n.detectLanguage ( text, function( detected )
-		{
-			// pass detected language into ouput variable
-			lang.detected = detected.languages[0].language;
-
-			// determine local variation of language based on the browser UI in BCP-47 standard
-			lang.local =  ( RiddR.locale.substring(0,2) == lang.detected )? RiddR.locale : lang.detected;
-			
-			// set reading language
-			lang.read = ( lang.requested != 'auto' )? lang.requested : lang.local;
-
-			// send the detected language
-			callback( lang );
-		})
-	}
+	locale 		= chrome.i18n.getUILanguage(); // get UI locale
 
 /*
  * ---------------------------------------------------------------------------------------------------------------------
  * Translate text from one into another language
  * ---------------------------------------------------------------------------------------------------------------------
 */	
-	this.translate = function ( text, from, to, callback )
-	{
-		callback(text);
-	}
+	lang 		= this.#detect; // get UI locale
+	translate 	= this.#translate; // translate text/utterance to specific language
+
+
 /*
  * ---------------------------------------------------------------------------------------------------------------------
- * Get language from it's ISO code
+ * Translate text from one into another language
  * ---------------------------------------------------------------------------------------------------------------------
 */	
-	this.lang_from_code = function ( code )
+	constructor ()
 	{
-		if ( RiddR.data.languages[code] != undefined )
-			return Object.assign ( RiddR.data.languages[code], { code : code } );
-		
-		else if ( RiddR.data.languages[code.substr(0,2)] != undefined) // failback to official if dialect is not found 
-			return Object.assign ( RiddR.data.languages[code.substr(0,2)], { code : code.substr(0,2) } );
-
-		else
-		{
-			RiddR.log('Requested language code: ' + code + ' is not in RiddR language list.', 'warn');
-			return false;
-		}
+		// return the proxied 
+		return new Proxy( this, this.handler() );
 	}
-/*
- * ---------------------------------------------------------------------------------------------------------------------
- * Localize all DOM i18n elements
- * ---------------------------------------------------------------------------------------------------------------------
-*/
-	var _localize = function()
-	{
-		elements = document.querySelectorAll('[i18n]')
 
-		for( index in elements )
-		{
-			if( elements.hasOwnProperty( index ) ) // avoid objects from prototype chain
-			{
-				if( message = RiddR.__( elements[index].getAttribute('i18n') )) // check if the i18n message exists
-				{
-					_update( message, elements[index] );
+	// storage magic router 
+	handler ()
+	{ 
+		return 	{
+					get: ( TARGET, NAME, RECEIVER ) => 
+					{
+						return this[NAME] ?? chrome.i18n.getMessage(NAME)
+					}
 				}
-			}
-		}
 	}
-	
 /*
  * ---------------------------------------------------------------------------------------------------------------------
- * Update DOM element with localized content
- * ---------------------------------------------------------------------------------------------------------------------
-*/
-	var _update = function( message, element )
-	{
-		if( element.innerHTML == '' || element.title != '' ) // update just the title if element content is empty eg. <i i18n='enquene'></i>
-			element.title = message;
-		else
-		{
-			// determine element type and update corresponding attribute / content
-			switch(element.tagName) 
-			{	
-				case 'INPUT':
-					element.value = message;
-				break;
-
-				case 'IMG':
-					element.src = message;
-				break;
-
-				default:
-					element.innerHTML = message;
-				break;
-			}
-
-			if(element.title != '') // check if element title needs to updated too 
-				element.title = message;
-		}
-	}
-
-/*
- * ---------------------------------------------------------------------------------------------------------------------
- * REGISTER EVENT LISTENERS
+ * Translate text from one into another language
  * 
- * Localize DOM content when DOM is fully loaded 
+ * Detect the language of the selected utterance and determine the reading language
  * ---------------------------------------------------------------------------------------------------------------------
-*/
-	RiddR.on('load', _localize);
+*/	
+	async #detect ( UTTERANCE, LANG = 'auto' )
+	{
+		const result = await chrome.i18n.detectLanguage( UTTERANCE )
 
-}).apply(RiddR);
+		// extract tje detected language
+		const [detected] = result.languages;
+
+		// return the language object 
+		return {
+					request: LANG,
+					detected: detected.language,
+					local: this.locale.startsWith( detected.language ) ? this.locale : detected.language,
+
+					// determine the reading language
+					get read(){ return ( this.request == 'auto' )? this.local : this.request }
+		}
+	}
+
+	// translate utterance to specified language
+	async #translate ( UTTERANCE, LANG )
+	{
+		return UTTERANCE;
+	}
+}
+
+export default new i18n();
