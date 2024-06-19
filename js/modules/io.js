0aa0db5823ebe4cb6a1cfa8f40560e7b461db81f
Trajche Petrov <ceo@qbit.solutions>
2024-06-19 22:21:13 +0200

    Added connection/channel tracking in the IO module
    - improved the connection/chhannel handling by adding channel management mechanisms which keep track of the state of the channels and their health
    - added simple routing mechanisms which are used for delivering particular message to the right communication channel

--- js/modules/io.js
+++ js/modules/io.js
@@ -1,5 +1,4 @@
 /*
- * IO  
  *
  * RiddR message broker used to enable comminication between RiddR internal modules and for external apps 
  *
@@ -13,70 +12,90 @@ if( typeof IO !== 'function' )
 {
 	class IO 
 	{
+		queue 		= []; // message queue
+		listeners 	= []; // registered event listeners from IO.on method
+		channels 	= []; // communication channels
 
-		port 		= null;
-		context 	= null;
-		listeners 	= [];
-		queue 		= [];
-
+		UUID 		= crypto.randomUUID(); // context channel ID
+		lastCH 		= null; // active channel ID
+		channel 	= null; // last used communication channel
+		context 	= this.#context(); // context environment in which the IO script is executing
 /*
  * ---------------------------------------------------------------------------------------------------------------------
  * Define 
  * ---------------------------------------------------------------------------------------------------------------------
 */	
-		constructor()
+		constructor( DOM )
 		{
-			// determine the context mode 
-			this.context = this.#context();
 
 			// dynamicaly register background message handlers  
-			if( this.context === 'background' )
+			if( this.#in('background') )
 				chrome.runtime.onConnect.addListener( this.connect.bind(this) );
-			else // open communication channel
+
+			else if( !this.#in('content') ) // open communication channel by default in all other contexts 
 				this.connect();
+
+			// if DOM is present register event listener for channel disconnection on reloading window
+			if( DOM )
+			{
+				window.onbeforeunload 	= this.disconnect.bind(this);
+				window.onpageshow = this.reconnect.bind(this)
+
+			}
+
 		}
 
+		// send 
+		init( ) { this.emit( 'load', this.context ); }
 /*
  * ---------------------------------------------------------------------------------------------------------------------
  *  Public IO methods
  * 
  *  Initialize the extension by dispatcing the on.load event
  * ---------------------------------------------------------------------------------------------------------------------
-*/	
-		init( )
-		{
-			this.dispatch( 'load', [], this.context );
-		}
-
+*/
 		// register multimodal event listener
-		on ( event, callback )
+		on ( EVENT, CALLBACK )
 		{
 			// create empty event opject if not allreayd registered 
-			this.listeners[event] = this.listeners[event] || []
+			this.listeners[EVENT] ??= []
 
 			// push the event and its callback to the event object
-			this.listeners[event].push( callback )
+			this.listeners[EVENT].push( CALLBACK )
 		}
 
-		// dispatch event to specific target
-		dispatch ( event, args, target )
+		// syntax sugar for shorthad event emitting
+		// eg. IO.emit('event'), ...('event', 'target'), ...('event', { object }, 'target' )
+		emit( EVENT, DATA, TARGET )
+		{
+			this.dispatch
+			( 
+				EVENT, 
+				typeof DATA !== 'object' ? {} : DATA, 
+				typeof DATA === 'string' ? DATA : TARGET ?? 'background'
+			);
+		}
+
+		// dispatch message to specific target
+		dispatch ( EVENT, ARGS, TARGET )
 		{
 			// set message 
 			let message = 
 			{ 
-				event: event, 
-				data: args ?? [], 
-				target: target, 
-				sender: this.context 
+				event 	: EVENT, 
+				data 	: ARGS ?? [], 
+				target 	: TARGET, 
+				sender 	: this.context,
+				senderID: this.UUID
 			};
 
 			// send message to the other extension modules
-			if( target != null )
+			if( TARGET != null )
 			{
-				if( target != this.context )
+				if( TARGET != this.context )
 				{
-					if( this.port !== null ) 
-						this.port.postMessage( message ); 	// send the message directly
+					if( this.#ready( message ) )
+						this.#send( message )
 					else
 						this.queue.push(message); 			// or add it to the queue for delayed delivery
 				}
@@ -85,60 +104,87 @@ if( typeof IO !== 'function' )
 			}
 		}
 
-		// syntax sugar for shorthad event emitting
-		// eg. IO.emit('event'), ...('event', 'target'), ...('event', { object }, 'target' )
-		emit( EVENT, DATA, TARGET )
+		// determine the right receiver and send the message
+		#send ( MESSAGE )
 		{
-			console.log(EVENT);
+			// check if targeted communication exists
+			if( this.channels[MESSAGE.target] )
+				this.channels[MESSAGE.target].postMessage( MESSAGE );
+			
+			// if content script is targeted, target the one in active tab
+			else if( MESSAGE.target === 'content' && this.channels[this.lastCH] )
+				this.channels[this.lastCH].postMessage( MESSAGE );
 
-			this.dispatch
-			( 
-				EVENT, 
-				typeof DATA !== 'object' ? {} : DATA, 
-				typeof DATA === 'string' ? DATA : TARGET ?? 'global'
-			);
-		}
+			// broadcast the message to all channels
+			else if( MESSAGE.target === 'global' )
+				for ( const channel in this.channels )
+					this.channels[channel].postMessage( MESSAGE );
 
-		// message handler 
-		handler( request, sender )
-		{
-			// execute local registered event listeners
-			if( ( request.target == this.context || request.target == 'global' ) && this.listeners[request.event] )
-					this.listeners[request.event].forEach( callback => callback( request.data ?? [] ) );
-		}
-
-		// open communuication channels between the extension contexts 
-		connect ( PORT = null )
-		{
-			// attach to an existing or create new communication channel			
-			this.port = ( PORT === null ) ? chrome.runtime.connect( {name: "RiddR"} ) : PORT;
-
-			// register message handker
-			this.port.onMessage.addListener( this.handler.bind(this) );;
-
-			// register channel disconection 
-			this.port.onDisconnect.addListener( () => { this.port = null } );
-
-			// process queued messages
-			this.queued();
-		}
-
-
-		// process delayed/queued messages
-		queued ()
-		{
-			// send queued messages once the channel is established
-			this.queue.forEach( ( message ) => { 
-													this.port.postMessage( message ) 
-												});
-
-			// reset the queue as the messages has been sent
-			this.queue = [];
+			// failback to the last open channel
+			else
+				this.channel.postMessage( MESSAGE );
 		}
 
 /*
  * ---------------------------------------------------------------------------------------------------------------------
- *  Provider IO helpers
+ *  Public IO methods
+ * 
+ *  Open communuication channels between the extension contexts 
+ * ---------------------------------------------------------------------------------------------------------------------
+*/
+		connect ( CHANNEL = null, TEST = null )
+		{
+			if( !this.channels[this.UUID] )
+			{
+				// attach to an existing or create new communication channel			
+				this.channel = CHANNEL || chrome.runtime.connect( {name: this.#getID() } );
+
+				// keep trakc of the opened channels
+				this.channels[this.channel.name] = this.channel;
+
+				// register message handlers
+				this.channel.onMessage.addListener( this.handler.bind(this) );;
+				this.channel.onDisconnect.addListener( this.onDisconnect.bind(this) );
+
+				// process queued messages
+				this.queued();
+			}
+
+			return this.UUID;
+		}
+
+		// hange the UUID to allow re-connection
+		reconnect ( EVENT )
+		{
+			if( EVENT.persisted )
+				this.UUID = crypto.randomUUID();
+		}
+
+		// message handler 
+		handler( REQUEST, SENDER )
+		{
+			// execute local registered event listeners
+			if( ( REQUEST.target == this.context || REQUEST.target == 'global' ) && this.listeners[REQUEST.event] )
+					this.listeners[REQUEST.event].forEach( callback => callback( REQUEST.data ?? [] ) );
+		}
+
+		// 
+		onDisconnect ( CHANNEL )
+		{ 
+			this.channel = null;
+
+			delete this.channels[CHANNEL.name];
+		}
+
+		// 
+		disconnect ()
+		{
+			this.channel.disconnect();
+		}
+
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ *  IO private method helpers
  * 
  *  determine the contenxt in which the IO module is located
  * ---------------------------------------------------------------------------------------------------------------------
@@ -155,18 +201,67 @@ if( typeof IO !== 'function' )
 			
 			return 'content';
 		}
+
+		// validation for the right execution context
+		#in ( CONTEXT )
+		{
+			return this.context === CONTEXT;
+		}
+
+		// determine if the communication channel for particular message delivery is ready
+		#ready ( MESSAGE )
+		{
+			if 	( 	// if the message target exists or the target is global
+					this.channels[ MESSAGE.target ] || MESSAGE.target == 'global' ||
+
+					// or sending communication channel is open
+					MESSAGE.senderID == this.channel?.name || MESSAGE.sender == this.channel?.name 
+				)
+					return true;
+
+			// validate if the target has channel ready
+			if( MESSAGE.target === 'content' && ( this.lastCH && this.channels[this.lastCH] ) )
+				return true;
+
+			// altertnatevly push the message to the queue
+			return false;
+		}
+
+		// get channel ID/name 
+		#getID ()
+		{
+			if( this.context === 'content' )
+				return this.UUID;
+
+			return this.context;
+		}
+
+		// process delayed/queued messages
+		queued ()
+		{
+			// send queued messages once the channel is established
+			this.queue.forEach( ( message ) => { 
+													this.#send( message ) 
+												});
+
+			// reset the queue as the messages has been sent
+			this.queue = [];
+		}
 	}
 
 /*
  * ---------------------------------------------------------------------------------------------------------------------
- *  Provider IO helpers
+ *  IO modile instantiation
  * 
- *  determine the contenxt in which the IO module is located
+ *  Determine the contenxt in which the IO module is located and make the propper instantiation
+ *  this allows the module to be run in service worker as ES modules via facades or IIFE modules
  * ---------------------------------------------------------------------------------------------------------------------
 */
 	if( typeof ServiceWorkerGlobalScope === 'function' )
 		self.IO = new IO() // instantiate and pass it to the serviceworker for using it as facade
 	else
+	{
 		// alternatevley the IO module is loaded in non-ES module context
-		window.IO = new IO();
+		window.IO = new IO( true );
+	}
 }
