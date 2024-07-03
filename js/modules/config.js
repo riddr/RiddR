da49be0548a85b30c508529c888a11fb66313796
Trajche Petrov <ceo@qbit.solutions>
2024-07-03 17:38:57 +0200

    Added CONFIG capability for nested object updates handling
    - this allows actions to CONFIG.property.nested to be properly propagated into the browser storage provider

--- js/modules/config.js
+++ js/modules/config.js
@@ -13,7 +13,7 @@ if( typeof CONFIG !== 'function' )
 {
 	class CONFIG 
 	{
-		// 
+		// config storage container
 		#cache = {};
 
 		// set RiddR default options
@@ -47,54 +47,101 @@ if( typeof CONFIG !== 'function' )
 		// define the storage provider
 		// using sync in an effort to sync riddr user settings accross multiple browsers
 		provider = chrome.storage.sync;
-
-		// instantiate the storage controller
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Register various i18n methods
+ * 
+ * Instantiate the storage controller
+ * ---------------------------------------------------------------------------------------------------------------------
+*/	
 		constructor ()
 		{
 			// sync storage data
-			this.sync();
+			this.#sync();
 
 			// register data syncing event listener
-			chrome.storage.onChanged.addListener( this.sync.bind(this) )
+			chrome.storage.onChanged.addListener( this.#sync.bind(this) )
 
 			// return the proxied 
-			return new Proxy( this, this.handler() );
+			return new Proxy( this, this.#handler );
 		}
 
-		// storage magic router
-		handler ()
-		{ 
-			return 	{
-						get: ( TARGET, NAME, RECEIVER ) => 
-						{
-							return this.#cache[NAME];
-						},
+		// synchronize stored data from chrome storagee
+		#sync ()
+		{
+			this.provider.get().then( DATA => 
+			{
+				this.#cache = { ... this.#defaults, ... this.#cache, ... DATA };
 
-						set: ( TARGET, NAME, VALUE, RECEIVER ) =>
-						{
-							this.#cache[NAME] = VALUE;
+				// trigger a local event that the configuration storage has been loaded
+				if( arguments.length === 0 )
+					IO.emit( 'config_ready', IO.context );
+			})
+		}
+
+/*
+ * ---------------------------------------------------------------------------------------------------------------------
+ * PROXY handler
+ * 
+ * Registers basic proxy traps which enables us neat object handling
+ * ---------------------------------------------------------------------------------------------------------------------
+*/	
+		#handler =	
+		{
+			get: ( TARGET, NAME, RECEIVER ) => 
+			{
+				// return recursive proxy in case of nested objects
+				if( typeof this.#cache[NAME] === 'object' )
+					return new Proxy( this.#cache[NAME], this.#handler.nested )
+
+				return this.#cache[NAME];
+			},
+
+			set: ( TARGET, NAME, VALUE, RECEIVER ) =>
+			{
+				this.#cache[NAME] = VALUE;
+
+				return this.provider.set( this.#cache );
+			},
+
+			deleteProperty: ( TARGET, PROPERTY ) =>
+			{
+				delete this.#cache[ PROPERTY ];
+
+				return this.provider.remove( PROPERTY );
+			},
+
+			ownKeys: ( TARGET ) =>
+			{
+				return Reflect.ownKeys( this.#defaults ); 
+			},
+
+			getOwnPropertyDescriptor: ( TARGET, NAME ) =>
+			{
+				return {
+					configurable: true,
+					enumerable: true,
+				};
+			},
+
+			// OBJECT Proxy handler
+			// used for updating changes in nested objects 
+			nested: {
+						set: ( TARGET, NAME, VALUE ) => 
+						{ 
+							Reflect.set( TARGET, NAME, VALUE );
 
 							return this.provider.set( this.#cache );
 						},
 
 						deleteProperty: ( TARGET, PROPERTY ) =>
 						{
-							delete this.#cache[ PROPERTY ];
+							Reflect.deleteProperty( TARGET, PROPERTY );
 
-							return this.provider.remove( PROPERTY );
+							return this.provider.set( this.#cache );
 						}
 					}
 		}
-
-		// synchronize stored data from chrome storagee
-		sync ()
-		{
-			this.provider.get().then( DATA => 
-			{
-				this.#cache = { ... this.#defaults, ... this.#cache, ... DATA };
-			})
-		}
-
 	}
 
 	// instantiate and pass it to the serviceworker for using it as facade
